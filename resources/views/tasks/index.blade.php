dff64851f15175bfbc95e6f8f8f03d9171a75e35
Arthur Bufon <arthurbufon2052@gmail.com>
2023-06-06 21:17:15 -0300

    adicionado template e preparando listagem

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Lista de Tarefas')
+
+@section('header', 'Listando Tarefas')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-5" style="margin:auto">
+            <div class="card">
+                <div class="card-header bg-primary">Título Tarefa 1</div>
+                <div class="card-body">Corpo Tarefa 1</div>
+            </div>
+        </div>
+    </div>
+@endsection

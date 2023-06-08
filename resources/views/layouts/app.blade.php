bdca22c2a8b5805f94295f613c10df78282298e7
Arthur <arthurbufon2052@gmail.com>
2023-06-08 14:19:39 -0300

    database dump

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
         <!-- Main Sidebar Container -->
         <aside id="sidebar" class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="{{ route('dashboard') }}" class="brand-link">
+            <a href="https://github.com/ArthurBufon/laravel10_todolist" class="brand-link">
                 <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">ADS - UPF</span>
